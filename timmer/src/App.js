aeb5315a0fdec769e786a93d3564d0f8f4babf10
ingasafranova <sfrnv@yahoo.co.uk>
2023-07-14 10:53:06 +0100

    finished work

--- timmer/src/App.js
+++ timmer/src/App.js
@@ -1,259 +1,247 @@
-import React, {  useState } from "react";
-import useSound from "use-sound";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect, useRef } from "react";
 
+const App = () => {
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+  const [timeLeft, seTtimeLeft] = useState(1500);
+  const [timingType, setTimingtype] = useState("SESSION");
+  const [play, setPlay] = useState(false);
 
-function App() {
-
-  const soundUrl = "https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav";
-
-  //to make the state 25 min need to multiply 25*60 sec
-const [breakLength, setBreakLength] = useState(2);
-const [sessionLength, setSessionLength] = useState(3) ;
-const [displayTime, setDisplayTime] = useState(5);
-const [timerOn, setTimerOn] = useState(false);
-const [onBreak, setOnBreak] = useState(false);
-//to get audio file from folder
- const [play,{pause}] = useSound(soundUrl);
-
-
-const formatTime = (displayTime) => {
-
-  let minutes = Math.floor(displayTime / 60);
-  let seconds = displayTime - minutes *60;
-  return (
-    (minutes < 10 ? "0" + minutes : minutes) +
-    ":" +
-    (seconds < 10 ? "0" + seconds : seconds)
-  );
-};
-
-
-
- const changeTime = (amount, type) => {
-    if (type === "break") {
-      //check if time not going to be less than 60sec
-      if (breakLength <= 1 && amount < 0) {
-        return;
-      }
-      setBreakLength((prev) => prev + amount);
-    } else {
-      if (sessionLength <= 1 && amount < 0) {
-        return;
-      }
-      setSessionLength((prev) => prev + amount);
-      if (!timerOn) {
-        setDisplayTime(displayTime + amount);
-      }
+  const handleBreakIncrease = () => {
+    if (breakLength < 60) {
+      setBreakLength(breakLength + 1);
     }
   };
-function controlTimer() {
-  //make seconds to miliseconds
-  let second = 1000;
-  //get accurate time
-  let date = new Date().getTime();
-  let nextDate = new Date().getTime() + second;
-  let onBreak = false;
 
-  if (!timerOn) {
-    let interval = setInterval(() => {
-      date = new Date().getTime();
-      if (date > nextDate) {
-        setDisplayTime((prev) => {
+  const handleBreakDecrease = () => {
+    if (breakLength > 1) {
+      setBreakLength(breakLength - 1);
+    }
+  };
 
-          if (prev <= 0 && !onBreak) {
-            
-            play.currentTime = 0;
-  play();
-            onBreak = true;
-            setOnBreak(true);
-            // setSessionLength(25);
-            return breakLength;
-          } else if (prev <= 0 && onBreak) {
-            play.currentTime = 0;
-            play();
-            onBreak = false;
-            setOnBreak(false);
-            //setSessionLength(25);
-            return sessionLength;
-          }
-          return prev - 1;
-        });
-        nextDate += second;
-      }
-      //interval set to 30 miliseconds to make timer more accurate
-    }, 30);
-    //store id to clear interval
-    localStorage.clear();
-    localStorage.setItem("interval-id", interval);
-  }
-  if (timerOn) {
-    clearInterval(localStorage.getItem("interval-id"));
-  }
-  setTimerOn(!timerOn);
+  const handleSessionIncrease = () => {
+    if (sessionLength < 60) {
+      setSessionLength(sessionLength + 1);
+      seTtimeLeft(timeLeft + 60);
+    }
+  };
 
-}
-function resetTimer() {
-  setDisplayTime(1500);
-  setBreakLength(5 );
-  setSessionLength(25 );
-  setTimerOn(false);
-  clearInterval(localStorage.getItem("interval-id"));
-  resetAudio();
-}
-function resetAudio() {
-  pause();
-}
+  const handleSessionDecrease = () => {
+    if (sessionLength > 1) {
+      setSessionLength(sessionLength - 1);
+      seTtimeLeft(timeLeft - 60);
+    }
+  };
+
+  const tick = () => {
+    if (play) {
+      seTtimeLeft(timeLeft - 1);
+    }
+  };
+
+  let timerRef = useRef();
+
+  const handleReset = () => {
+    clearInterval(timerRef);
+    setPlay(false);
+    seTtimeLeft(1500);
+    setBreakLength(5);
+    setSessionLength(25);
+    setTimingtype("SESSION");
+    const audio = document.getElementById("beep");
+    audio.pause();
+    audio.currentTime = 0;
+  };
+
+  const handlePlay = () => {
+    setPlay(!play);
+  };
+
+  const resetTimer = () => {
+    const audio = document.getElementById("beep");
+    if (!timeLeft && timingType === "SESSION") {
+      seTtimeLeft(breakLength * 60);
+      setTimingtype("BREAK");
+      audio.play();
+    }
+    if (!timeLeft && timingType === "BREAK") {
+      seTtimeLeft(sessionLength * 60);
+      setTimingtype("SESSION");
+      audio.play();
+      audio.currentTime = 0;
+    }
+  };
+
+  useEffect(() => {
+    timerRef.current = setInterval(tick, 1000);
+    resetTimer();
+    return () => clearInterval(timerRef.current);
+  }, [tick, resetTimer]);
+
+  const timeFormatter = () => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft - minutes * 60;
+    const formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
+    const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+    return `${formattedMinutes}:${formattedSeconds}`;
+  };
+
+  const title = timingType === "SESSION" ? "Session" : "Break";
 
   return (
-    <div className="App">
-      <h1>25 + 5 Clock</h1>
-      <div className="container">
-      <BreakLenght
-        title={"Break Length"}
-        changeTime={changeTime}
-        type={"break"}
-        time={breakLength}
-        
-      />
-      <SessionLenght
-        title={"Session Length"}
-        changeTime={changeTime}
-        type={"session"}
-        time={sessionLength}
-        
-      />
-      <Timer
-        displayTime={displayTime}
-        timerOn={timerOn}
-        controlTimer={controlTimer}
-        resetTimer={resetTimer}
-        title={"Session" || "Break"}
-        formatTime={formatTime}
-        ref={play}
-      
-      />
-     </div>
-    </div> 
-  );
-  function BreakLenght({title, changeTime, type}) {
-    return (
-      <div>
-        <h3 id="break-label">{title}</h3>
-        <div className="time-sets">
-          <button id="break-decrement" className="btn-level" value="-" onClick={()=>changeTime(-1,type)}>
-            <i className="fa fa-arrow-down fa-2x"></i>
-          </button>
-           <h3 id="break-length">{breakLength}</h3> 
-          <button id="break-increment" className="btn-level" value="+" onClick={()=>changeTime(+1, type)}>
-            <i className="fa fa-arrow-up fa-2x"></i>
-          </button>
+    <main>
+      <h2>25 + 5 Clock</h2>
+      <div className="settings">
+        <div className="break-session-length">
+          <h3 id="break-label">Break Length</h3>
+
+          <div className="controls-container">
+            <button
+              className="btn"
+              disabled={play}
+              onClick={handleBreakIncrease}
+              id="break-increment"
+              value="+"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </button>
+            <strong id="break-length">{breakLength}</strong>
+            <button
+              className="btn"
+              disabled={play}
+              onClick={handleBreakDecrease}
+              id="break-decrement"
+              value="-"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div className="session-container">
+          <h3 id="session-label">Session Length</h3>
+          <div className="controls-container">
+            <button
+              className="btn"
+              disabled={play}
+              onClick={handleSessionIncrease}
+              id="session-increment"
+              value="+"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </button>
+            <strong id="session-length">{sessionLength}</strong>
+            <button
+              className="btn"
+              disabled={play}
+              onClick={handleSessionDecrease}
+              id="session-decrement"
+              value="-"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
-    );
-  }
-  function SessionLenght({title, changeTime, type}) {
-    return (
-      <div>
-        <h3 id="session-label">{title}</h3>
-        <div className="time-sets">
-          <button id="session-decrement" className="btn-level" value="-" onClick={()=>changeTime(-1,type)}>
-            <i className="fa fa-arrow-down fa-2x"></i>
-          </button>
-           <h3 id="session-length">{sessionLength}</h3> 
-          <button id="session-increment" className="btn-level" value="+" onClick={()=>changeTime(+1, type)}>
-            <i className="fa fa-arrow-up fa-2x"></i>
-          </button>
+      <div className="timer-wrapper">
+        <div className="timer">
+          <h2 id="timer-label">{title}</h2>
+          <h3   id="time-left">{timeFormatter()}</h3>
         </div>
-      </div>
-    );
-  }
-
-function Timer({displayTime,controlTimer,resetTimer,onBreak, formatTime}) {
-  
-  return (
-    <div className="timer-wrap"> 
-    <div className="timer-container">
-      <h3 id="timer-label">{onBreak?  "Session" : "Break"}</h3>
-      <h2 id="time-left">{formatTime(displayTime)}</h2>
-      </div>
-      <div className="timer-control">
-        <button id="start_stop" onClick={controlTimer } >
-          <i className="fa fa-play fa-2x"></i>
-          <i className="fa fa-pause fa-2x"></i>
-          <audio
-          id="beep"
-          preload="auto"
-          type="audio/mpeg"
-          
-          src ="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"/>
+        <div className="buttons">
+        <button className="btn" onClick={handlePlay} id="start_stop">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 7.5V18M15 7.5V18M3 16.811V8.69c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 010 1.954l-7.108 4.061A1.125 1.125 0 013 16.811z"
+            />
+          </svg>
         </button>
-        <button id="reset" onClick={resetTimer}>
-          <i className="fa fa-refresh fa-2x"></i>
+        <button className="btn" onClick={handleReset} id="reset">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+            />
+          </svg>
         </button>
-        
       </div>
-  </div>
+</div>
+      <audio
+        id="beep"
+        preload="auto"
+        src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+      />
+    </main>
   );
-  }
-
-
-
-
-
-
-}
-
+};
 export default App;
-
-// function App() {
-//   const [breakLength, setBreakLength] = React.useState(5 * 60);
-
-//   //to make the state 25 min need to multiply 25*60 sec
-//   const [sessionLength, setSessionLength] = React.useState(25 * 60);
-//   // const [timerMinute, setTimerMinute] = React.useState(25);
-//   // const [intervalId, setIntervalId] = React.useState(null);
-//   // const [currentTimer, setCurrentTimer] = React.useState('Session');
-//   // const [isPlay, setIsPlay] = React.useState(false);
-//   // const [timerSecond, setTimerSecond] = React.useState(0);
-
-//   const formatTime = (time) => {
-//     let minutes = Math.floor(time / 60);
-//     let seconds = time % 60;
-//     return (
-//       (minutes < 10 ? "0" + minutes : minutes) +
-//       ":" +
-//       (seconds < 10 ? "0" + seconds : seconds)
-//     );
-//   };
-//   return (
-//     <div className="App">
-//       <h1>25 + 5 Clock</h1>
-//       <BreakLenght
-//         title={"Break Length"}
-//         changeTime={null}
-//         type={"break"}
-//         time={breakLength}
-//         formatTime={formatTime}
-//       />
-//       <h2>{formatTime(sessionLength)}</h2>
-//     </div>
-//   );
-//   function BreakLenght(title, changeTime, type, time, formatTime) {
-//     return (
-//       <div>
-//         <h3 id="break-label">{title}</h3>
-//         <div className="time-sets">
-//           <button id="break-decrement" className="btn-level" value="-">
-//             <i className="fa fa-arrow-down fa-2x"></i>
-//           </button>
-//           <h3 id="break-length">{formatTime(time)}</h3>
-//           <button id="break-increment" className="btn-level" value="+">
-//             <i className="fa fa-arrow-up fa-2x"></i>
-//           </button>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
